5eb3e607776083d7e8bc9b1996c8280f8666b946
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-07-03 13:01:26 +0200

    cleaned up example usage

--- src/example-usage.js
+++ src/example-usage.js
@@ -5,13 +5,11 @@ const sampleEdits = require('../sample-data/sample-input/edits.json');
 const result = generateEDL({
 	projectOriginator: 'Digital Paper Edit',
 	edits: sampleEdits,
-	filePaths: [ '/some/path/to/the/file/1', '/some/path/to/the/file/2' ],
-	// fileNames,
 	// https://en.wikipedia.org/wiki/Sampling_(signal_processing)#Sampling_rate
 	sampleRate: '44100',
 	frameRate: 25,
 	projectName: 'Test edit 1'
 });
 
-fs.writeFileSync('./sample-data/sample-output/example-output.adl.edl', result);
+fs.writeFileSync('./sample-data/sample-output/example-output.adl', result);
 console.log(result);
